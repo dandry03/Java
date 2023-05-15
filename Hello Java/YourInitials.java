2406417a6d8c6892cc9c0ea833e83760eb4ab10b
dandry03 <72827204+dandry03@users.noreply.github.com>
2023-05-15 14:22:40 -0500

    Add files via upload

---
+++ Hello Java/YourInitials.java
@@ -0,0 +1,16 @@
+public class YourInitials {
+    public static void main(String[] args) {
+         // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+        System.out.println("DDDD      A");
+        System.out.println("D   D    A A ");
+        System.out.println("D   D   A   A");
+        System.out.println("D   D   AAAAA");
+        System.out.println("D   D   A   A");
+        System.out.println("D   D   A   A");
+        System.out.println("DDDD    A   A");
+
+
+
+
+    }
+}
